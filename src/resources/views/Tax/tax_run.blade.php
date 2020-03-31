f981d40da925d30f59b77a64aa0f77b635011318
tomideo1 <ayotomideaina@gmail.com>
2020-03-31 02:17:09 +0100

    updated the tax run

--- src/resources/views/Tax/tax_run.blade.php
+++ src/resources/views/Tax/tax_run.blade.php
@@ -173,12 +173,15 @@
 
         function processRows(row,index) {
             row.created_at = moment(row.created_at).format('DD MMM, YYYY');
-            if(row.status !== 'processed'){
+            if( row.status === 'running'){
+                row.buttons =
                 '<a class="btn btn-warning" data-status="'+row.isActive+'" data-index="'+index+'"  data-action="edit_run" data-id="'+row.id+'" data-name="'+row.name+'">Update</a> &nbsp;';
-
             }
-            row.buttons =
-                '<a class="btn btn-sm btn-primary text-white"  href="/mfn/tax-run/total/authorities/' + row.id + '">View Tax  Authorities Slip</a> &nbsp;';
+            else{
+                row.buttons =
+                    '<a class="btn btn-sm btn-primary text-white"  href="/mfn/tax-run/total/authorities/' + row.id + '">View Tax  Authorities Slip</a> &nbsp;';
+            }
+
 
 
         }
